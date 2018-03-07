9153644bcf713dd8e397647d8f0da08a0522c8ef
Ivan Malison <IvanMalison@gmail.com>
2018-03-06 19:48:49 -0800

    Finish type conversion function for client generation

--- lib/DBus/Generate.hs
+++ lib/DBus/Generate.hs
@@ -1,35 +1,57 @@
 {-# LANGUAGE TemplateHaskell #-}
 module DBus.Generate where
 
-import           DBus
 import           DBus.Client as C
 import qualified DBus.Internal.Message as M
 import qualified DBus.Internal.Types as T
 import qualified DBus.Introspection as I
 import           Data.Coerce
+import           Data.Int
 import           Data.List
+import qualified Data.Map as Map
 import           Data.String
+import           Data.Word
 import           Language.Haskell.TH
+import           System.Posix.Types (Fd(..))
 
 data GenerationParams = GenerationParams
-  { genBusName :: Maybe BusName
-  , genTypeToName :: T.Type -> Name
+  { genBusName :: Maybe T.BusName
+  , getTHType :: T.Type -> Type
   }
 
-defaultTypeToName t =
+defaultGetTHType :: T.Type -> Type
+defaultGetTHType t =
   case t of
-    T.TypeString -> ''String
-    T.TypeInt32 -> ''Int
+    T.TypeBoolean -> ConT ''Bool
+    T.TypeWord8 -> ConT ''Word8
+    T.TypeWord16 -> ConT ''Word16
+    T.TypeWord32 -> ConT ''Word32
+    T.TypeWord64 -> ConT ''Word64
+    T.TypeInt16 -> ConT ''Int16
+    T.TypeInt32 -> ConT ''Int32
+    T.TypeInt64 -> ConT ''Int64
+    T.TypeDouble -> ConT ''Double
+    T.TypeUnixFd -> ConT ''Fd
+    T.TypeString -> ConT ''String
+    T.TypeSignature -> ConT ''T.Signature
+    T.TypeObjectPath -> ConT ''T.ObjectPath
+    T.TypeVariant -> ConT ''T.Variant
+    T.TypeArray arrayType -> AppT ListT $ defaultGetTHType arrayType
+    T.TypeDictionary k v -> (AppT (AppT (ConT ''Map.Map)
+                                          (defaultGetTHType k))
+                             (defaultGetTHType v))
+    T.TypeStructure ts -> foldl AppT (TupleT $ length ts) $ map defaultGetTHType ts
 
+defaultGenerationParams :: GenerationParams
 defaultGenerationParams =
   GenerationParams
   { genBusName = Nothing
-  , genTypeToName = defaultTypeToName
+  , getTHType = defaultGetTHType
   }
 
 generateMethodCall :: GenerationParams -> String -> I.Method -> Q [Dec]
 generateMethodCall GenerationParams
-                     { genTypeToName = typeToName
+                     { getTHType = getArgType
                      } interfaceNameString
                    I.Method
                      { I.methodArgs = args
@@ -40,12 +62,13 @@ generateMethodCall GenerationParams
         outputLength = length outputArgs
         buildArgNames = mapM (newName . I.methodArgName) inputArgs
         buildOutputNames = mapM (newName . I.methodArgName) outputArgs
-    clientName <- newName "client"
-    busName <- newName "busName"
-    objectPath <- newName "objectPath"
-    variants <- newName "variants"
-    methodCallArg <- newName "methodCall"
-    callResultName <- newName "callResult"
+    clientN <- newName "client"
+    busN <- newName "busName"
+    objectPathN <- newName "objectPath"
+    variantsN <- newName "variants"
+    methodCallN <- newName "methodCall"
+    callResultN <- newName "callResult"
+    replySuccessN <- newName "replySuccess"
     methodArgNames <- buildArgNames
     fromVariantOutputNames <- buildOutputNames
     finalOutputNames <- buildOutputNames
@@ -64,22 +87,25 @@ generateMethodCall GenerationParams
         maybeExtractionPattern = mapOrHead makeJustPattern finalOutputNames TupP
         getFunctionBody = [|
              do
-               let $( varP variants ) = $( return $ ListE variantListExp )
-                   $( varP methodCallArg ) =
-                     (methodCall $( return $ VarE objectPath )
+               let $( varP variantsN ) = $( return $ ListE variantListExp )
+                   $( varP methodCallN ) =
+                     (methodCall $( return $ VarE objectPathN )
                                    (fromString interfaceNameString)
                                    (fromString methodString))
-                     { M.methodCallDestination = $( return $ VarE busName )
-                     , M.methodCallBody = $( return $ VarE variants )
+                     { M.methodCallDestination = Just $( return $ VarE busN )
+                     , M.methodCallBody = $( return $ VarE variantsN )
                      }
-               $( return $ VarP callResultName ) <- call $( return $ VarE clientName )
-               return $ case $( return $ VarE callResultName ) of
-                          $( return $ ListP $ map VarP fromVariantOutputNames) ->
-                            case $( return $ fromVariantExp ) of
-                              $( return maybeExtractionPattern ) ->
-                                $( return finalResultTuple )
-                              _ -> ReplyError errorInvalidParameters []
-                          _ -> ReplyError errorInvalidParameters []
+               $( return $ VarP callResultN ) <- call $( return $ VarE clientN )
+                                                           $( varE methodCallN )
+               return $ case $( varE callResultN ) of
+                 Right $( varP replySuccessN ) ->
+                   case (M.methodReturnBody $( varE replySuccessN )) of
+                     $( return $ ListP $ map VarP fromVariantOutputNames) ->
+                       case $( return $ fromVariantExp ) of
+                         $( return maybeExtractionPattern ) -> Right $( return finalResultTuple )
+                         _ -> Left C.errorInvalidParameters
+                     _ -> Left C.errorInvalidParameters
+                 Left _ -> Left C.errorInvalidParameters
            |]
     functionBody <- getFunctionBody
     let methodSignature = foldr addInArg fullOutputSignature inputArgs
@@ -87,19 +113,18 @@ generateMethodCall GenerationParams
         addInArg arg = addArg $ getArgType $ I.methodArgType arg
         fullOutputSignature = AppT (ConT ''IO) $
                               AppT (AppT (ConT ''Either)
-                                         (ConT ''M.MethodError))
+                                         (ConT ''T.ErrorName))
                               outputSignature
         outputSignature =
           case outputLength of
             1 -> getArgType $ I.methodArgType $ head outputArgs
             _ -> foldl addOutArg (TupleT outputLength) outputArgs
         addOutArg target arg = AppT target $ getArgType $ I.methodArgType arg
-        getArgType = ConT . typeToName
-        functionName = (mkName methodString)
+        functionN = (mkName methodString)
         fullSignature = addArg (ConT ''C.Client) $
                         addArg (ConT ''T.BusName) $
                         addArg (ConT ''T.ObjectPath) methodSignature
-        fullArgNames = clientName:busName:objectPath:methodArgNames
-        definitionDec = SigD functionName fullSignature
-        function = FunD functionName [Clause (map VarP fullArgNames) (NormalB functionBody) []]
+        fullArgNames = clientN:busN:objectPathN:methodArgNames
+        definitionDec = SigD functionN fullSignature
+        function = FunD functionN [Clause (map VarP fullArgNames) (NormalB functionBody) []]
     return [definitionDec, function]
